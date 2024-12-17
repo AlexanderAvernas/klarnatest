e5fc7d533ca5e615787b768804d00b883f2284ca
Alexander-Avernas <AlexanderAvernas@users.noreply.github.com>
2024-12-17 15:48:57 +0100

    test if tsl works from mail transpoter

--- src/app/utils/mail.js
+++ src/app/utils/mail.js
@@ -2,10 +2,12 @@ import nodemailer from 'nodemailer';
 
 // Setup av e-posttransport via Gmail SMTP
 const transporter = nodemailer.createTransport({
-    service: 'gmail', // Använder Gmail som e-posttjänst
+    host: 'smtp.gmail.com',
+    port: 587, // Använd 587 för TLS
+    secure: false, // false = TLS (STARTTLS), true = SSL
     auth: {
-        user: process.env.EMAIL_USER, // Din Gmail-adress från .env
-        pass: process.env.EMAIL_PASS, // App-lösenord från .env
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
     },
 });
 
